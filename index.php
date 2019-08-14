6cc29fdab7e65642d1862f6aec59434890164032
bian0726 <a2425241732@gmail.com>
2019-08-14 12:04:16 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+!<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bob_Bian MessageBoard</title>
+  </head>
+  <body>
+    <form class="" method="post">
+      <input type="text" name="userName" id="userName">
+      <input type="text" name="userMessage" id="userMessage">
+      <input type="submit" name="btnOK" id="btn">
+    </form>
+  </body>
+</html>
